c17012eb53f1e2811bab889f337b90228bf9b1c2
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-04-08 17:11:07 +0530

    Coupon Number

--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Welcome to Logical Program!");
+Console.WriteLine("Welcome to Coupon Number !");
+Random random = new Random();
+
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+var stringChars = new char[6];
+
+for (int i = 0; i < stringChars.Length; i++)
+{
+    stringChars[i] = chars[random.Next(chars.Length)];
+    Console.WriteLine(" Coupon Number " + stringChars[i]);
+}
